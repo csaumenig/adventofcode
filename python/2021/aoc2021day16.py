3ac804ef7bb7948860fd593c5dc93817d17b0e0e
Chris Saumenig <chris.saumenig@gmail.copm>
2022-01-07 18:02:37 -0500

    1. Getting closer with day 16 - still having problems with the recursion in certain cases

--- python/2021/aoc2021day16.py
+++ python/2021/aoc2021day16.py
@@ -5,66 +5,62 @@ DAY = 16
 
 class Packet:
     def __init__(self,
-                 version: int,
+                 packet_version: int,
                  packet_type: int) -> None:
-        self._version = version
-        self._packet_type = packet_type
-        self._literal = 0
-        self._length_type_id = ''
+        self._packet_version: int = packet_version
+        self._packet_type: int = packet_type
+        self._length_type_id: str = ''
+        self._literal: int = 0
         self._sub_packets: list[Packet] = []
+        self._value: int = 0
 
     @property
-    def version(self) -> int:
-        return self._version
+    def packet_version(self) -> int:
+        return self._packet_version
 
     @property
     def packet_type(self) -> int:
         return self._packet_type
 
     @property
-    def length_type(self) -> int:
-        return self._length_type
+    def length_type_id(self) -> str:
+        return self._length_type_id
 
-    def set_literal(self, literal: int) -> None:
+    @property
+    def literal(self) -> int:
+        return self._literal
+
+    def set_literal(self,
+                    literal: int) -> None:
         self._literal = literal
 
-    def set_length_type_id(self, length_type_id: str) -> None:
+    def set_length_type_id(self,
+                           length_type_id: str) -> None:
         self._length_type_id = length_type_id
 
-    def add_sub_packet(self, sub_packet: Packet) -> None:
+    def add_sub_packet(self,
+                       sub_packet: Packet) -> None:
         self._sub_packets.append(sub_packet)
 
     def version_total(self) -> int:
-        return self._version + sum([packet.version for packet in self._sub_packets])
+        return self._version + sum([p.packet_version for p in self._sub_packets])
+
+    @property
+    def value(self) -> int:
+        if self._value == 0:
 
 
-convert_dict = {
-    '0': '0000',
-    '1': '0001',
-    '2': '0010',
-    '3': '0011',
-    '4': '0100',
-    '5': '0101',
-    '6': '0110',
-    '7': '0111',
-    '8': '1000',
-    '9': '1001',
-    'A': '1010',
-    'B': '1011',
-    'C': '1100',
-    'D': '1101',
-    'E': '1110',
-    'F': '1111'
-}
 
 
 def part1(file_name_str: str) -> None:
+    print('***********************')
+    print(f'Source File = {file_name_str}')
     hex_str = read_file(file_name_str)
-    print(f'Hex Str = {hex_str}')
-    bits = convert(hex_str)
-    print(f'Bit Str = {bits}')
-    total_version, remainder = read_packet(bits, 0)
+    bits = hex_to_bin(hex_str)
+    total_version, remainder, rem_len = read_packet(bits, 0)
     print(f'Day {DAY} Part 1: Total of versions = {total_version}')
+    print('***********************')
+    print('')
 
 
 def part2(file_name_str: str) -> None:
@@ -81,7 +77,6 @@ def read_file(file_name_str: str) -> str:
 
 def read_packet(packet_string: str,
                 total_version: int) -> tuple[int, str, int]:
-    print(f'packet_string = {packet_string}')
     packet_version = bin_to_dec(packet_string[:3])
     packet_type = bin_to_dec(packet_string[3:6])
     total_version += packet_version
@@ -89,29 +84,44 @@ def read_packet(packet_string: str,
 
     if packet_type == 4:
         literal, remainder, r_l = read_literal(packet_string[6:])
-        return total_version, remainder, remove_length + r_l
+        remove_length += r_l
+        return total_version, remainder, remove_length
 
     packet_length_type_id = packet_string[6:7]
+    length_of_sub_packets = 0
     length_of_remaining_sub_packets = 0
+    number_of_sub_packets = 0
     number_of_remaining_sub_packets = 0
+    sub_length = 0
 
     if packet_length_type_id == '0':
+        length_of_sub_packets = bin_to_dec(packet_string[7:22])
         length_of_remaining_sub_packets = bin_to_dec(packet_string[7:22])
-        string_to_consider = packet_string[22:22 + length_of_remaining_sub_packets]
+        string_to_consider = packet_string[22:22+length_of_sub_packets]
+        remove_length += 16
     else:
+        number_of_sub_packets = bin_to_dec(packet_string[7:18])
         number_of_remaining_sub_packets = bin_to_dec(packet_string[7:18])
         string_to_consider = packet_string[18:]
+        remove_length += 12
 
-    while length_of_remaining_sub_packets > 0 or number_of_remaining_sub_packets > 0:
+    while len(string_to_consider) > 0 and (length_of_remaining_sub_packets > 0 or number_of_remaining_sub_packets > 0):
         total_version, remainder, r_l = read_packet(string_to_consider, total_version)
-        remove_length += r_l
         if length_of_remaining_sub_packets > 0:
             packet_length = len(string_to_consider) - len(remainder)
             length_of_remaining_sub_packets -= packet_length
             string_to_consider = remainder
         elif number_of_remaining_sub_packets > 0:
             number_of_remaining_sub_packets -= 1
-            string_to_consider = string_to_consider[remove_length:]
+            sub_length += r_l
+            string_to_consider = string_to_consider[r_l:]
+
+    if length_of_sub_packets > 0:
+        remove_length += length_of_sub_packets
+        packet_string = packet_string[remove_length:]
+    elif number_of_sub_packets > 0:
+        remove_length += sub_length
+        packet_string = packet_string[remove_length:]
 
     return total_version, packet_string, remove_length
 
@@ -132,9 +142,20 @@ def read_literal(packet_str: str) -> tuple[int, str, int]:
     return bin_to_dec(literal_str), packet_str, remove_length
 
 
-def convert(hex_string: str) -> str:
-    converted = [convert_dict.get(c) for c in hex_string]
-    return ''.join(converted)
+def hex_to_bin(hex_string: str) -> str:
+    return_val = ''
+    for h in hex_string:
+        dec_val = int(h, 16)
+        bin_val = ''
+        remainder = dec_val
+        for i in range(3, -1, -1):
+            if remainder // (2 ** i) == 1:
+                bin_val += '1'
+                remainder -= 2 ** i
+            else:
+                bin_val += '0'
+        return_val += bin_val
+    return return_val
 
 
 def bin_to_dec(bin_string) -> int:
@@ -147,19 +168,21 @@ def bin_to_dec(bin_string) -> int:
 
 
 if __name__ == '__main__':
-    #file_name = f'../../resources/{YEAR}/inputd{DAY}a.txt'
-    #part1(file_name)
+    file_name = f'../../resources/{YEAR}/inputd{DAY}a.txt'
+    part1(file_name)
     #part2(file_name)
-    #file_name = f'../../resources/{YEAR}/inputd{DAY}b.txt'
-    #part1(file_name)
+    file_name = f'../../resources/{YEAR}/inputd{DAY}b.txt'
+    part1(file_name)
     #part2(file_name)
     file_name = f'../../resources/{YEAR}/inputd{DAY}c.txt'
     part1(file_name)
-    part2(file_name)
-    #
-    # file_name = f'../../resources/{YEAR}/inputd{DAY}.txt'
-    # part1(file_name)
     # part2(file_name)
-    # bins = ['100', '101', '110', '111']
-    # for b in bins:
-    #     print(f'b: {b} = {bin_to_dec(b)}')
+    file_name = f'../../resources/{YEAR}/inputd{DAY}d.txt'
+    part1(file_name)
+    # part2(file_name)
+    file_name = f'../../resources/{YEAR}/inputd{DAY}e.txt'
+    part1(file_name)
+    # part2(file_name)
+    file_name = f'../../resources/{YEAR}/inputd{DAY}.txt'
+    part1(file_name)
+    # part2(file_name)
