7f29aa6c12e71cf8c217fef7d546882959fdbd08
trpgm54 <christopher.saumenig@troweprice.com>
2022-01-16 11:22:56 -0500

    1. Day 15 and Dijkstra example

--- python/2021/aoc2021day15.py
+++ python/2021/aoc2021day15.py
@@ -1,179 +1,107 @@
-import copy
-import math
-import types
-from enum import Enum
+from __future__ import annotations
+from queue import PriorityQueue
 
 YEAR = 2021
 DAY = 15
 
 
-class CellMark(Enum):
-    No = 0
-    Start = 1
-    End = 2
-
-
-class Cell:
+class Graph:
     def __init__(self,
-                 value: int,
-                 pos: tuple[int, int] = None):
-        self.count = 0
-        self.value = value
-        self.mark = CellMark.No
-        self.pos = pos
-        self.path_from = None
+                 num_of_rows,
+                 num_of_cols):
+        self.rows = num_of_rows
+        self.cols = num_of_cols
+        self.grid = [[-1 for i in range(num_of_rows)] for j in range(num_of_cols)]
+        self.visited = []
+        self.neighbors: dict[tuple[int, int], list[tuple[int, int]]] = {}
 
+    def add_risk(self, r, c, risk) -> None:
+        self.grid[r][c] = risk
+        self.add_neighbors(r, c)
 
-class CellGrid:
-    def __init__(self, board):
-        self.board = board
-        self.rows = 0
-        self.cols = 0
-        for o in self.board:
-            if o.pos[0] > self.cols:
-                self.cols = o.pos[0]
-            if o.pos[1] > self.rows:
-                self.rows = o.pos[1]
-        self.rows += 1
-        self.cols += 1
+    def add_neighbors(self, r, c) -> None:
+        my_neighbors: list[tuple[int, int]] = []
+        if r > 0:
+            my_neighbors.append((r - 1, c))
+        if r < self.rows:
+            my_neighbors.append((r + 1, c))
+        if c > 0:
+            my_neighbors.append((r, c - 1))
+        if c < self.cols:
+            my_neighbors.append((r, c + 1))
+        self.neighbors.update({(r, c): my_neighbors})
 
-    def at(self, pos: tuple[int, int]):
-        for o in self.board:
-            if o.pos == pos:
-                return o
-
-    def clone(self):
-        return CellGrid(copy.deepcopy(self.board))
-
-    def clear_count(self, count):
-        for o in self.board:
-            o.count = count
-            o.path_from = None
-
-    def is_valid_point(self,
-                       pos: tuple[int, int]):
-        return pos[0] in range(0, self.rows) and pos[1] in range(0, self.cols)
-
-    def __repr__(self):
-        output = ''
+    def expand(self,
+               num_times: int) -> None:
         for r in range(0, self.rows):
-            line = ''
-            if output.strip() != '':
-                output += '\n'
-            for c in range(0, self.cols):
-                line += str(self.at((c, r)).value)
-            output += line
-        return output
 
 
-def create_empty_map(x: int,
-                     y: int,
-                     start: tuple[int, int],
-                     end: tuple[int, int]):
-    return types.SimpleNamespace(
-        board=CellGrid([[Cell(pos=(ix, iy), value=0) for iy in range(y)] for ix in range(x)]),
-        start=start,
-        end=end)
+
+    @staticmethod
+    def from_file(file: str) -> Graph:
+        tmp_grid = read_file(file)
+        rows = len(tmp_grid)
+        columns = len(tmp_grid[0])
+        graph = Graph(rows, columns)
+        for row in range(rows):
+            for column in range(columns):
+                graph.add_risk(row, column, tmp_grid[row][column])
+        return graph
 
 
-def create_map_from_grid(grid: dict[tuple[int, int], int],
-                         start: tuple[int, int],
-                         end: tuple[int, int]):
-    return types.SimpleNamespace(
-        board=CellGrid([Cell(pos=(k[1], k[0]), value=v) for k, v in grid.items()]),
-        start=start,
-        end=end)
+def part1(f: str) -> None:
+    graph = Graph.from_file(f)
+    risk_dict = dijkstra(graph, (0, 0))
+    print(f'Day {DAY} Part 1: Lowest Total Risk: {risk_dict.get((graph.rows - 1, graph.cols - 1))}')
+    # print(risk_dict)
 
 
-def add_point(a: tuple[int, int],
-              b: tuple[int, int]) -> tuple[int, int]:
-    return a[0] + b[0], a[1] + b[1]
+def part2(f: str) -> None:
+    graph = Graph.from_file(f)
 
 
-def find_least_risk_path(board: CellGrid,
-                         start: tuple[int, int],
-                         end: tuple[int, int],
-                         max_distance=math.inf):
-    nboard = board.clone()
-    nboard.clear_count(math.inf)
 
-    # mark the start and end for the UI
-    nboard.at(start).mark = CellMark.Start
-    nboard.at(end).mark = CellMark.End
+def dijkstra(graph: Graph,
+             start: tuple[int, int]):
+    risk_dict = {(row, column): float('inf') for row in range(graph.rows) for column in range(graph.cols)}
+    risk_dict[start] = 0
 
-    # we start here, thus a distance of 0
-    open_list = [start]
-    nboard.at(start).count = 0
+    pq = PriorityQueue()
+    pq.put((0, start))
 
-    # (x,y) offsets from current cell
-    neighbours = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    while open_list:
-        cur_pos = open_list.pop(0)
-        cur_cell = nboard.at(cur_pos)
+    while not pq.empty():
+        (distance, current_point) = pq.get()
+        graph.visited.append(current_point)
 
-        for neighbour in neighbours:
-            ncell_pos = add_point(cur_pos, neighbour)
-            if not nboard.is_valid_point(ncell_pos):
-                continue
-            cell = nboard.at(ncell_pos)
-            dist = cur_cell.count + 1
-            if dist > max_distance:
-                continue
-
-            if cell.count > dist:
-                cell.count = dist
-                cell.path_from = cur_cell
-                open_list.append(ncell_pos)
-    return nboard
+        for x in range(graph.rows):
+            for y in range(graph.cols):
+                if (x, y) in graph.neighbors.get(current_point):
+                    if (x, y) not in graph.visited:
+                        risk = graph.grid[x][y]
+                        old_risk = risk_dict.get((x, y))
+                        new_risk = risk_dict.get(current_point) + risk
+                        if new_risk < old_risk:
+                            pq.put((new_risk, (x, y)))
+                            risk_dict.update({(x, y): new_risk})
+    return risk_dict
 
 
-def backtrack_to_start(board: CellGrid,
-                       end: tuple[int, int]):
-    """
-    Returns the path to the end, assuming the board has been filled in via fill_shortest_path
-    """
-    cell = board.at(end)
-    path = []
-    while cell is not None:
-        path.append(cell)
-        cell = cell.path_from
-    return path
-
-
-def part1(file_name_str: str) -> None:
-    grid, rows, cols = read_file(file_name_str)
-    start = (0, 0)
-    end = (cols - 1, rows - 1)
-    my_map = create_map_from_grid(grid, start, end)
-    filled = find_least_risk_path(my_map.board, start, end)
-    print(filled)
-    print(f'Day {DAY} Part 1: ANSWER')
-
-
-def part2(file_name_str: str) -> None:
-    grid, rows, cols = read_file(file_name_str)
-    print(f'Day {DAY} Part 2: ANSWER')
-
-
-def read_file(file_name_str: str) -> tuple[dict[tuple[int, int], int], int, int]:
-    grid: dict[tuple[int, int], int] = {}
-    r = 0
-    c = 0
+def read_file(file_name_str: str) -> list[list[int]]:
+    grid: list[list[int]] = []
     lines = [line.strip() for line in open(file_name_str, 'r').readlines()]
     for line in lines:
-        c = 0
-        for char in line:
-            grid.update({(r, c): int(char)})
-            c += 1
-        r += 1
-    return grid, r, c
+        this_line = []
+        for risk in line:
+            this_line.append(int(risk))
+        grid.append(this_line)
+    return grid
 
 
 if __name__ == '__main__':
     file_name = f'../../resources/{YEAR}/inputd{DAY}a.txt'
     part1(file_name)
-    part2(file_name)
-
-    # file_name = f'../../resources/{YEAR}/inputd{DAY}.txt'
-    # part1(file_name)
     # part2(file_name)
+
+    file_name_str = f'../../resources/{YEAR}/inputd{DAY}.txt'
+    part1(file_name_str)
+    # part2(file_name_str)
---
+++ python/reference/djikstra.py
@@ -0,0 +1,97 @@
+nodes = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
+distances = {
+    'B': {'A': 5, 'D': 1, 'G': 2},
+    'A': {'B': 5, 'D': 3, 'E': 12, 'F': 5},
+    'D': {'B': 1, 'G': 1, 'E': 1, 'A': 3},
+    'G': {'B': 2, 'D': 1, 'C': 2},
+    'C': {'G': 2, 'E': 1, 'F': 16},
+    'E': {'A': 12, 'D': 1, 'C': 1, 'F': 2},
+    'F': {'A': 5, 'E': 2, 'C': 16}
+}
+
+unvisited = {node: None for node in nodes}  # using None as +inf
+visited = {}
+current = 'B'
+currentDistance = 0
+unvisited[current] = currentDistance
+
+while True:
+    for neighbour, distance in distances[current].items():
+        if neighbour not in unvisited:
+            continue
+        newDistance = currentDistance + distance
+        if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
+            unvisited[neighbour] = newDistance
+    visited[current] = currentDistance
+    del unvisited[current]
+    if not unvisited:
+        break
+    candidates = [node for node in unvisited.items() if node[1]]
+    current, currentDistance = sorted(candidates, key=lambda x: x[1])[0]
+
+print(visited)
+
+
+
+
+# Dijkstra's Algorithm in Python
+import sys
+
+# Function to find which vertex is to be visited next
+def next_to_visited():
+    global visited
+    v = -10
+    for index in range(num_of_vertices):
+        if visited[index][0] == 0 and (v < 0 or visited[index][1] <= visited[v][1]):
+            v = index
+    return v
+
+# Function to find the shortest path between nodes in a graph
+def dijkstra():
+    for vertex in range(num_of_vertices):
+
+        # Find next vertex to be visited
+        visit = next_to_visited()
+
+        for index in range(num_of_vertices):
+
+            # Updating new distances
+            if vertices[visit][index] == 1 and visited[index][0] == 0:
+                new_distance = visited[visit][1] + edges[visit][index]
+                if visited[index][1] > new_distance:
+                    visited[index][1] = new_distance
+
+            visited[visit][0] = 1
+
+# Printing the distance
+def print_dist():
+    i = 0
+    for distance in visited[1:]:
+print("Distance from S ->", chr(ord('BZ!    ZZŽå')+i), ":",distance[1])
+        i = i + 1
+
+# Driver Code
+# Providing the graph
+# 2D array to represnt the vertex structure
+vertices = [[0, 1, 1, 1],
+            [1, 0, 1, 0],
+            [1, 1, 0, 1],
+            [1, 0, 1, 0]]
+
+# 2D array to represnt the edge structure
+edges = [[0, 1, 2, 5],
+        [1, 0, 2, 0],
+        [2, 2, 0, 4],
+        [5, 0, 4, 0]]
+
+# calculate Number of vertices
+num_of_vertices = len(vertices[0])
+
+# store the visited edge and vertex
+visited = [[0, 0]]
+for i in range(num_of_vertices-1):
+    visited.append([0, sys.maxsize])
+
+# Function calling
+dijkstra()
+print_dist()
